a9c2c9f29e6832e54f5c7c84a3235e25d4615fd3
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-11 23:41:25 -0800

    Implement create action

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,5 +11,21 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+  
 end
