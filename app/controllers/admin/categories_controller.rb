9190ec17536f9966d40c6063184de81de6caf55e
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-11 22:55:22 -0800

    Implement index, new actions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,9 +3,11 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
