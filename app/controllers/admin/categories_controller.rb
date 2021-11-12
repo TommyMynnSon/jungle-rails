4dc34a6029418728dd5ab28a0e25813bc07964df
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-11 23:12:17 -0800

    Refactor Category query to fetch category records and order by ascending name column

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.order(name: :asc).all
   end
 
   def new
