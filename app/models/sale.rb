83660e3b05d53a8a0912247053e0fe0ac83aa6cf
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-13 14:47:22 -0800

    Add AR Scope/Class method for querying all active Sale records

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,12 @@
 class Sale < ActiveRecord::Base
 
+  # AR Scope ()
+  # scope :active, -> { where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current) }  
+
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+
   def finished?
     ends_on < Date.current
   end
