6bcec29cdeef2b3a235a185d133f486f65ed5ae3
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-16 09:12:24 -0800

    Define initial empty tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'something' do
+      
+    end
+
+    it 'something' do
+      
+    end
+
+    it 'something' do
+      
+    end
+
+    it 'something' do
+      
+    end
+
+    it 'something' do
+      
+    end
+  end
+
 end
