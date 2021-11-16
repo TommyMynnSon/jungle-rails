2e43ad13ceb196692459e1cf1087ab3719687b4b
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-16 12:04:40 -0800

    Add test to see if Product can be saved if all mandatory fields are set, Add test to see if Product is invalid for save if name field is not set

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,12 +3,31 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
 
   describe 'Validations' do
-    it 'something' do
+    it 'is valid to be saved if name, price/price_cents, quantity, and category are set' do
+      @category = Category.new(name: 'Refurbished')
       
+      @product = Product.new(
+                              name: 'Refurbished iPhone 13 PRO MAX 128GB',
+                              price: 500,
+                              quantity: 1,
+                              category: @category
+                            )
+        
+      expect(@product).to be_valid
     end
 
-    it 'something' do
+    it 'should not be valid if name is not set' do
+      @category = Category.new(name: 'Refurbished')
       
+      @product = Product.new(
+        price: 500,
+        quantity: 1,
+        category: @category
+      )
+
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'something' do
@@ -25,3 +44,14 @@ RSpec.describe Product, type: :model do
   end
 
 end
+
+# it "is valid" do
+#   @product = Product.new
+#   @cat = Category.new
+#   @cat.name = 'Test'
+#   @product.name = 'Test' # invalid state
+#   @product.price_cents = 12311
+#   @product.quantity = 3
+#   @product.category = @cat
+#   expect(@product.valid?).to be true
+# end
