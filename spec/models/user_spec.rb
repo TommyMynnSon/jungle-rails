af0548c2f3c3f1c7907d8f3380f9f10cdd18c947
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-17 03:12:04 -0800

    Test if User can be saved with required fields set

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'is valid to be saved if name, email, password, and password_confirmation are set' do
+      @user = User.new(
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: '0000',
+        password_confirmation: '0000'
+      )
+
+      expect(@user).to be_valid
+    end
+  end
+  
 end
