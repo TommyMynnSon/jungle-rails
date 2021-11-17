bdd835a660da257213dcfe2aa45f27f39dc22648
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-17 03:29:21 -0800

    Test if User is not valid if password and password_confirmation do not match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,6 +49,25 @@ RSpec.describe User, type: :model do
 
       expect(@user.errors.full_messages).to be_empty
     end
+
+    it 'should not be valid if password and password_confirmation do not match' do
+      @user = User.new(
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: '0000',
+        password_confirmation: '1111'
+      )
+
+      @user.valid?
+
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+
+      @user.password_confirmation = '0000'
+
+      @user.valid?
+
+      expect(@user.errors.full_messages).to be_empty
+    end
   end
   
 end
