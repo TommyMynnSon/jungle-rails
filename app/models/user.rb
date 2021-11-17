a435479f88b6e7f9a6308cf020843aaa4609a8f2
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-17 03:58:35 -0800

    Enforce presence and uniqueness of email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password_confirmation, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email
 
 end
