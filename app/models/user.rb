6fd8f377aa4b704b4dcac25ab487d930af3cdf1a
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-17 05:42:24 -0800

    Enforce minimum length 2 for password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :password, length: { minimum: 2 }
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
